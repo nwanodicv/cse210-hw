56c98cbcc77d96b843ac70d3aef813f8d66739d1
nwanodicv <victornwanodi@gmail.com>
2025-01-09 17:37:57 +0100

    finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args)// Main
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("");// This is for empty line
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();// Prompt user to add first name
+        Console.Write("what is your last name? ");
+        string lastName = Console.ReadLine();// Prompt user to add last name
+
+        Console.WriteLine("");// This is for empty line
+        // This displays user input on the screen
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        
+        Console.WriteLine("");// This is for empty line
+        
     }
 }
