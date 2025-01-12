4f0e529b5ba549413031aaa63fdbe87d48cc3db4
nwanodicv <victornwanodi@gmail.com>
2025-01-12 01:29:00 +0100

    update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+        
+        Console.WriteLine($"What is the magic number? {randomNumber}");// Magic Number
+
+        Console.Write("What is your guess? ");// User input guess
+        int userGuessed = int.Parse(Console.ReadLine());// parse the user input to integer
+
+        if (userGuessed == randomNumber)// Conditions for the magic number
+        {
+            Console.WriteLine("Congratulations! You guessed it.");// Output for the magic number
+        }
+
+        else if (userGuessed > randomNumber)// Conditions for the magic number
+        {
+            Console.WriteLine("High! Try again.");
+        }
+        else if (userGuessed < randomNumber)// Conditions for the magic number
+        {
+            Console.WriteLine("low! Try again.");// Output for the magic number
+        }
+
+        while (userGuessed != randomNumber)
+        {
+            Console.Write("What is your guess? ");// User input guess
+            userGuessed = int.Parse(Console.ReadLine());// parse the user input to integer
+
+            if (userGuessed == randomNumber)// Conditions for the magic number
+            {
+                Console.WriteLine("Congratulations! You guessed it.");// Output for the magic number
+            }
+            else if (userGuessed > randomNumber)// Conditions for the magic number
+            {
+                Console.WriteLine("High! Try again.");
+            }
+            else if (userGuessed < randomNumber)// Conditions for the magic number
+            {
+                Console.WriteLine("low! Try again.");// Output for the magic number
+            }
+        }
     }
 }
